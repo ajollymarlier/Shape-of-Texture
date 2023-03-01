f800d418245dbfc050dda71059df01b9d615940b
Arun Jolly Marlier <arunjm00@gmail.com>
2023-02-28 21:04:34 -0500

    revert map scale

--- Assets/PlayerInteract/Pressable.cs
+++ Assets/PlayerInteract/Pressable.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;// Required when using Event data.
 
-public class Pressable : Interactable
+public class Pressable : Interactable, IPointerEnterHandler
 {
-
     private bool isPressed;
 
     private void Start()
     {
         isPressed = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        Debug.Log("Enter!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        Debug.Log("Exit!");
     }
 
     public override void Interact()
@@ -18,17 +26,5 @@ public class Pressable : Interactable
             
         ButtonPuzzleInit buttonPuzzleInit = gameObject.transform.parent.GetComponent<ButtonPuzzleInit>();
         buttonPuzzleInit.handleButtonPress(gameObject);
-        // isPressed = !isPressed;
-        // if (isPressed)
-        // {
-        //     Debug.Log("Object is now pressed");
-            
-        //     ButtonPuzzleInit buttonPuzzleInit = gameObject.transform.parent.GetComponent<ButtonPuzzleInit>();
-        //     buttonPuzzleInit.handleButtonPress(gameObject);
-        // }
-        // else
-        // {
-        //     Debug.Log("Object is now unpressed");
-        // }
     }
 }
