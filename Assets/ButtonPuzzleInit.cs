f6e147706d672ee033b4cecd74136a3d3ebb6ddf
kolbychong <kolbychong314@gmail.com>
2023-03-14 01:48:31 -0400

    Main menu + mistake sfx

--- Assets/ButtonPuzzleInit.cs
+++ Assets/ButtonPuzzleInit.cs
@@ -12,61 +12,70 @@ public class ButtonPuzzleInit : MonoBehaviour
 {
     public GameObject[] orderedButtons;
     private int i;
+    private bool inProgress;
     // Start is called before the first frame update
     void Start()
     {
         i=0;
+        inProgress = false;
     }
 
     // Update is called once per frame
     public void init()
     {
         orderedButtons[0].GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        inProgress = true;
     }
 
     public void handleButtonPress(GameObject pressedButton){
-        if (i < orderedButtons.Length - 1 && pressedButton == orderedButtons[i]){
-            pressedButton.transform.GetChild(0).GetComponent<Light>().intensity = 0;
-            pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
-            i += 1;
+        if (inProgress){
+            if (i < orderedButtons.Length - 1 && pressedButton == orderedButtons[i]){
+                pressedButton.transform.GetChild(0).GetComponent<Light>().intensity = 0;
+                pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+                i += 1;
 
-            orderedButtons[i].GetComponent<FMODUnity.StudioEventEmitter>().Play();
-        
-        } 
-        // Wrong button
-        else if (pressedButton != orderedButtons[i])
-        {
-            pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
-            orderedButtons[i].GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+                orderedButtons[i].GetComponent<FMODUnity.StudioEventEmitter>().Play();
+            
+            } 
+            // Wrong button
+            else if (pressedButton != orderedButtons[i])
+            {
+                pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+                orderedButtons[i].GetComponent<FMODUnity.StudioEventEmitter>().Stop();
 
-            // Turn lights back on
-            for (int j=0; j < i; j++){
-                orderedButtons[j].transform.GetChild(0).GetComponent<Light>().intensity = 100;
+                // Wrong sound
+                FMODUnity.RuntimeManager.PlayOneShot("event:/GLOBAL events/SFX_Failure", transform.position);
+
+                // Turn lights back on
+                for (int j=0; j < i; j++){
+                    orderedButtons[j].transform.GetChild(0).GetComponent<Light>().intensity = 100;
+                }
+
+                i = 0;
+                orderedButtons[0].GetComponent<FMODUnity.StudioEventEmitter>().Play();
             }
+            else{
+                inProgress = false;
+                pressedButton.transform.GetChild(0).GetComponent<Light>().intensity = 0;
+                pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
 
-            i = 0;
-            orderedButtons[0].GetComponent<FMODUnity.StudioEventEmitter>().Play();
-        }
-        else{
-            pressedButton.transform.GetChild(0).GetComponent<Light>().intensity = 0;
-            pressedButton.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+                //Turn door lights back on
+                GameObject[] doorLights = GameObject.FindGameObjectsWithTag("Door Light");
+                for (int j=0; j < doorLights.Length; j++){
+                    doorLights[j].GetComponent<Light>().intensity = 40;
+                }
 
-            //Turn door lights back on
-            GameObject[] doorLights = GameObject.FindGameObjectsWithTag("Door Light");
-            for (int j=0; j < doorLights.Length; j++){
-                doorLights[j].GetComponent<Light>().intensity = 40;
+                //Turn Button Lights Off
+                GameObject[] buttonLights = GameObject.FindGameObjectsWithTag("Button Light");
+                for (int j=0; j < buttonLights.Length; j++){
+                    buttonLights[j].GetComponent<Light>().intensity = 0;
+                }
+
+                GameObject leftDoor = transform.Find("Left Door").gameObject;
+                leftDoor.SetActive(false);
+                // Play sound
+                FMODUnity.RuntimeManager.PlayOneShot("event:/Medical Bay Intro Scene/SFX_Doors_open", transform.position);
             }
-
-            //Turn Button Lights Off
-            GameObject[] buttonLights = GameObject.FindGameObjectsWithTag("Button Light");
-            for (int j=0; j < buttonLights.Length; j++){
-                buttonLights[j].GetComponent<Light>().intensity = 0;
-            }
-
-            GameObject leftDoor = transform.Find("Left Door").gameObject;
-            leftDoor.SetActive(false);
-            // Play sound
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Medical Bay Intro Scene/SFX_Doors_open", transform.position);
         }
     }
 }
