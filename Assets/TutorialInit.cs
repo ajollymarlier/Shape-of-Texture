62aedac8d72516b5a7f53c97b35067749714785f
kolbychong <kolbychong314@gmail.com>
2023-03-15 01:05:54 -0400

    tutorial bugfixes

--- Assets/TutorialInit.cs
+++ Assets/TutorialInit.cs
@@ -7,6 +7,7 @@ public class TutorialInit : MonoBehaviour
     public GameObject movingDoor;
     public bool isAudioLog;
     public string audioLogPath;
+    private FMODUnity.StudioEventEmitter emitter;
 
     private FMOD.Studio.EventInstance instance;
 
@@ -17,6 +18,10 @@ public class TutorialInit : MonoBehaviour
     public void handleObjPress(GameObject pressedObj){
         pressedObj.GetComponentInChildren<Light>().intensity = 0;
         movingDoor.transform.Translate(Vector3.right * 4);
+        if (!isAudioLog){
+            pressedObj.GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+        }
+        
 
         if (isAudioLog){
             instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
