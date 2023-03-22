1719911e46c49c5886e809c8c75056844e96918a
Arun Jolly Marlier <arunjm00@gmail.com>
2023-03-22 01:09:05 -0400

    added teleportation to last scene in final sequence

--- Assets/MiscScripts/MovementOverrideLogic.cs
+++ Assets/MiscScripts/MovementOverrideLogic.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovementOverrideLogic : MonoBehaviour
 {
     private bool triggered;
+    private bool teleported;
     public FirstPersonController player;
     public GameObject staticScreen;
     public Light flashlight;
+    public GameObject teleportMarker;
     public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
         triggered = false;
+        teleported = false;
     }
 
 
     void Update(){
-        if (triggered){
-            flashlight.intensity += 0.01f;
-            //Vector3.MoveTowards(player.transform.position, staticScreen.transform.position, moveSpeed * Time.deltaTime);
+        StartCoroutine(handleForcedMovement());
+    }
+
+    IEnumerator handleForcedMovement(){
+        if (triggered && !teleported){
             if (player.transform.position.z < staticScreen.transform.position.z){
                 player.transform.position += new Vector3(0, 0, moveSpeed * Time.deltaTime);
             }
@@ -28,6 +34,19 @@ public class MovementOverrideLogic : MonoBehaviour
             }
             else{
                 flashlight.GetComponent<FlashlightTimeout>().losingBattery = false;
+
+                 yield return new WaitForSeconds(4);
+                player.transform.position = teleportMarker.transform.position;
+                teleported = true;
+            }
+        }
+        else if (triggered && teleported){
+            if (GameObject.Find("Final Sequence Tele Location").transform.position.x - player.transform.position.x < 10){
+                player.transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            }
+            else{
+                yield return new WaitForSeconds(3);
+                SceneManager.LoadScene("Main_Menu");
             }
         }
     }
@@ -37,6 +56,7 @@ public class MovementOverrideLogic : MonoBehaviour
     {
         triggered = true;
         player.playerCanMove = false;
+        player.isWalking = false;
         player.enableHeadBob = false;
     }
 }
